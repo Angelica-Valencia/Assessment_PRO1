a9fa3605c5eb7ee5051c922e2e1cc445d7adcb68
Angelica Valencia <avalenciab.0108@gmail.com>
2023-02-17 12:01:49 +0800

    Modification to PlayerList with initial append method implememted that updates the head of the list

--- app/player_list.py
+++ app/player_list.py
@@ -1,9 +1,18 @@
 class PlayerList:
     def __init__(self):
         self._head = None
+        self._tail = None
 
     def is_empty(self):
         if self._head:
             return False
         else:
             return True
+
+    def get_head(self):
+        return self._head
+
+    def append(self, player):
+        if self.is_empty():
+            self._head = player
+
--- test/test_player_list.py
+++ test/test_player_list.py
@@ -7,8 +7,22 @@ class TestPlayerList(unittest.TestCase):
 
     def setUp(self) -> None:
         self.player = Player("1702", "Isabel")
+        self.player_2 = Player("1803", "Valeria")
         self.player_node = PlayerNode(self.player)
         self.player_list = PlayerList()
 
     def test_checking_empty_list_when_created(self):
         self.assertEqual(self.player_list.is_empty(), True)
+
+    def test_that_updates_the_head_of_the_list(self):
+        self.player_list.append(self.player)
+        self.assertEqual(self.player_list.is_empty(), False)
+
+    def test_that_checks_the_first_player_added_is_the_head(self):
+        self.player_list.append(self.player)
+        self.assertEqual(self.player_list.get_head(), self.player)
+
+    def test_that_checks_the_tail_is_still_the_first_player_added(self):
+        self.player_list.append(self.player)
+        self.player_list.append(self.player_2)
+        self.assertEqual(self.player_list.get_head(), self.player)
