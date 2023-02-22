cdee30b48574ae19d055b71c89e3e9a6451b2c04
Angelica Valencia <avalenciab.0108@gmail.com>
2023-02-22 15:08:00 +0800

    PlayerLis: Implementation of method that adds a player at the head when the list is empty and when is not.

--- app/player_list.py
+++ app/player_list.py
@@ -1,3 +1,7 @@
+from app.player_node import PlayerNode
+from app.player import Player
+
+
 class PlayerList:
     def __init__(self):
         self._head = None
@@ -12,10 +16,41 @@ class PlayerList:
     def get_head(self):
         return self._head
 
-    def append(self, player):
+    def add_at_the_head(self, player):
+        new_player_node = PlayerNode(player)
         if self.is_empty():
-            self._head = player
-            self._tail = player
+            self._head = new_player_node
         else:
-            pass
+            self._head.set_previous(new_player_node.get_player())
+            previous_head = self._head
+            new_player_node.set_next(previous_head)
+            self._head = new_player_node
 
+
+    # def append(self, player):
+    #     new_player_node = PlayerNode(player)
+    #     if self.is_empty():
+    #         self._head = new_player_node
+    #         self._tail = new_player_node
+    #     elif self._head == self._head:
+    #         self._head.set_next(new_player_node)
+    #         self._tail = new_player_node.
+    #     else:
+    #         node = self.
+
+if __name__ == "__main__":
+
+    my_list = PlayerList()
+    print(my_list.get_head(), my_list.is_empty(), "\n")
+
+    my_list.add_at_the_head(Player("123", "Lina"))
+    print(my_list.get_head(), my_list.is_empty(), "\n")
+
+    my_list.add_at_the_head(Player("456", "Eliza"))
+    print(my_list.get_head(), my_list.is_empty(), "\n")
+
+    my_list.add_at_the_head(Player("678", "Tania"))
+    print(my_list.get_head(), my_list.is_empty(), "\n")
+
+    my_list.add_at_the_head(Player("213", "Marce"))
+    print(my_list.get_head(), my_list.is_empty(), "\n")
--- test/test_player_list.py
+++ test/test_player_list.py
@@ -3,26 +3,51 @@ from app.player_node import PlayerNode
 from app.player import Player
 from app.player_list import PlayerList
 
+
 class TestPlayerList(unittest.TestCase):
 
     def setUp(self) -> None:
         self.player = Player("1702", "Isabel")
         self.player_2 = Player("1803", "Valeria")
-        self.player_node = PlayerNode(self.player)
+        self.player_3 = Player("7734", "Leonardo")
+
+        self.player_node_1 = PlayerNode(self.player)
+        self.player_node_2 = PlayerNode(self.player_2)
+        self.player_node_3 = PlayerNode(self.player_3)
+
         self.player_list = PlayerList()
 
     def test_checking_empty_list_when_created(self):
         self.assertEqual(self.player_list.is_empty(), True)
 
     def test_that_updates_the_head_of_the_list(self):
-        self.player_list.append(self.player)
+        self.player_list.add_at_the_head(self.player)
         self.assertEqual(self.player_list.is_empty(), False)
 
-    def test_that_checks_the_first_player_added_is_the_head(self):
-        self.player_list.append(self.player)
-        self.assertEqual(self.player_list.get_head(), self.player)
+    def test_that_checks_the_first_player_added(self):
+        self.player_list.add_at_the_head(self.player)
+        self.assertEqual(self.player_list.get_head().get_key(), self.player_node_1.get_key())
+        self.assertEqual(self.player_list.get_head().get_previous(), None)
+        self.assertEqual(self.player_list.get_head().get_next(), None)
 
-    def test_that_checks_the_tail_is_still_the_first_player_added(self):
-        self.player_list.append(self.player)
-        self.player_list.append(self.player_2)
-        self.assertEqual(self.player_list.get_head(), self.player)
+    def test_that_checks_player_added_when_list_is_not_empty(self):
+        self.player_list.add_at_the_head(self.player)
+        self.player_list.add_at_the_head(self.player_2)
+        self.assertEqual(self.player_list.get_head().get_key(), self.player_node_2.get_key())
+        self.assertEqual(self.player_list.get_head().get_previous(), None)
+        self.assertEqual(self.player_list.get_head().get_next().get_key(), self.player_node_1.get_key())
+
+    def test_that_checks_players_have_been_added_in_correct_order(self):
+
+        players = [self.player_node_3, self.player_node_2, self.player_node_1]
+
+        self.player_list.add_at_the_head(self.player)
+        self.player_list.add_at_the_head(self.player_2)
+        self.player_list.add_at_the_head(self.player_3)
+
+        node = self.player_list.get_head()
+
+        for player in players:
+            self.assertEqual(node.get_key(), player.get_key())
+            node = node.get_next()
+
